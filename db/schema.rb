3d94c175c7e3beabd9d18a50c965767ed6f6414e
Issei <chatani171@gmail.com>
2021-04-17 00:02:52 +0900

    不要なモデルやコントローラーを削除し、dbへのマイグレーションをリセット

--- db/schema.rb
+++ db/schema.rb
@@ -22,11 +22,4 @@ ActiveRecord::Schema.define(version: 2021_04_13_125500) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "title"
-    t.string "contents"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
